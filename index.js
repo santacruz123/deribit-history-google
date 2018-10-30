7c8541943288ff0f0804c3755f3008e4e772de80
Andrey Vorobyov <vorandrew@gmail.com>
2018-10-30 12:59:04 +0200

    IV

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const BigQuery = require('@google-cloud/bigquery')
 const axios = require('axios')
 const Promise = require('bluebird')
+const IV = require('implied-volatility')
+const moment = require('moment')
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-// const projectId = "your-project-id";
-// const datasetId = "my_dataset";
-// const tableId = "my_table";
-// const rows = [{name: "Tom", age: 30}, {name: "Jane", age: 32}];
+const bigquery = new BigQuery({ projectId: 'deribit-220920' })
 
-// Creates a client
-const bigquery = new BigQuery({
-  projectId: 'deribit-220920',
-})
+function calcIV(spot, dt, i, price) {
+  if (price === null) {
+    return null
+  }
+
+  const type = i[3] === 'C' ? 'call' : 'put'
+
+  const t = moment(i[1] + ' 09:00', 'DDMMMYY HH:mm').diff(moment.unix(dt), 'years', true)
+  const iv = IV.getImpliedVolatility(+price, +spot, +i[2], t, 0, type, 1)
+  return Math.round(iv * 10000) / 100
+}
 
 exports.history = (req, res) => {
   let dt
@@ -47,6 +50,20 @@ exports.history = (req, res) => {
         },
         { concurrency: 10 },
       )
+    })
+    .then(rows => {
+      let btcPrice = rows.filter(e => e.instrument === 'BTC-PERPETUAL')[0].bid
+
+      rows.forEach(e => {
+        let p = e.instrument.split('-')
+
+        if (p[0] === 'BTC' && p[3]) {
+          e.bid_iv = calcIV(btcPrice, dt, p, e.bid)
+          e.ask_iv = calcIV(btcPrice, dt, p, e.ask)
+        }
+      })
+
+      return rows
     })
     .then(rows => {
       return bigquery
